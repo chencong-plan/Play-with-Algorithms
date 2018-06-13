c61e3c56d40ad8627a2ad360320ef87cd6e7237a
chencong-plan <1042738887@qq.com>
2018-06-13 22:00:29 +0800

    HeapSort1 循环数组插入元素到堆 HeapSort2 直接O(n)构造data[n]的数组，然后对其进行构造二叉堆shiftDown(i)

--- 04-Heap/src/cc/ccoder/heapify/HeapSort2.java
+++ 04-Heap/src/cc/ccoder/heapify/HeapSort2.java
@@ -2,7 +2,12 @@ package cc.ccoder.heapify;
 
 public class HeapSort2 {
 
-    public static void sort(Comparable[] arr){
+    /**
+     * 排序，调用构造方法MaxHeap(T[] item) 产生一个堆
+     *
+     * @param arr 将要排序的堆
+     */
+    public static void sort(Comparable[] arr) {
         int length = arr.length;
         MaxHeap<Comparable> maxHeap = new MaxHeap<Comparable>(arr);
         for (int i = 0; i < length - 1; i++) {
@@ -11,10 +16,10 @@ public class HeapSort2 {
     }
 
     public static void main(String[] args) {
-        Integer[] arr = {1,3,4,9,4,2,1,0,6,4,8,23,11,3};
+        Integer[] arr = {1, 3, 4, 9, 4, 2, 1, 0, 6, 4, 8, 23, 11, 3};
         sort(arr);
         for (Integer item : arr) {
-            System.out.print(item+" ");
+            System.out.print(item + " ");
         }
     }
 }
